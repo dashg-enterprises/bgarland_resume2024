d361b8e4aa6104b7707d221a64f7a423069b337c
begarland <brandy.e.garland@gmail.com>
2024-11-09 21:43:07 -0700

    Update hosted zone

--- iac/spa-website.tf
+++ iac/spa-website.tf
@@ -13,7 +13,7 @@ module "spa_on_aws" {
   source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/spa-website/aws?ref=main" #"../../cloud-platform/src/modules/spa-website/aws"#"https://github.com/dashg-enterprises/cloud-platform.git//src/modules/aws/spa-website"
   dist_folder_path = "../dist"
   application_name = "bgarland-${var.environment}"
-  hosted_zone_id = "Z03829103FGKVJE6VHSAF"
+  hosted_zone_id = "Z1Z3EMCBHAG3LM"
   domain = "begarland.com"
   subdomain = var.environment_subdomain
 }
