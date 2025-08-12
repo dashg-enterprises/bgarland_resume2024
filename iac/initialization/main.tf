1b3243c3ce5753cbf8020528298372f861e56e62
Brandy Garland <brandy.e.garland@gmail.com>
2025-08-12 15:52:59 -0600

    remove terraform provider

--- iac/initialization/main.tf
+++ iac/initialization/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.59.0"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 module "spa_on_aws" {
   source = "git::https://github.com/dashg-enterprises/cloud-platform.git//modules/spa-website/aws/initialization?ref=main" #"../../cloud-platform/src/modules/spa-website/aws"#"https://github.com/dashg-enterprises/cloud-platform.git//src/modules/aws/spa-website"
   hosted_zone_id = "Z1Z3EMCBHAG3LM"
